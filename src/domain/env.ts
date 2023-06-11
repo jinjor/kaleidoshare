bf0cc8c1c14590eac6cc6924b8bdfed96da03dbc
Yosuke Torii <jinjorweb@gmail.com>
2023-06-12 01:35:23 +0900

    remove prod flag

--- src/domain/env.ts
+++ src/domain/env.ts
@@ -1,12 +1,8 @@
 export type Env = {
-  prod: boolean;
   os: "mac" | "other";
   mobile: boolean;
 };
 export const env: Env = {
-  prod: !["http://localhost:5173", "http://localhost:4173"].includes(
-    location.origin
-  ),
   os: navigator.userAgent.toLowerCase().includes("mac") ? "mac" : "other",
   mobile: "ontouchstart" in document.documentElement,
 };
